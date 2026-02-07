5e8ef9f660c1746a10b457b26fd43baad52d6161
malekrmadi <malekrmadi00@gmail.com>
2026-02-07 18:04:19 +0100

    new google sheet

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './Contact.css';
 
-const GOOGLE_APPS_SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbwSEfpeN83AU8wXJQ2bsDwGHA8UAr05uYnt8EzwxjbKgjlvalhApBXfcd-Ppq2R-lJraw/exec';
+//const GOOGLE_APPS_SCRIPT_URL =
+//  'https://script.google.com/macros/s/AKfycbwSEfpeN83AU8wXJQ2bsDwGHA8UAr05uYnt8EzwxjbKgjlvalhApBXfcd-Ppq2R-lJraw/exec';
+const GOOGLE_APPS_SCRIPT_URL ='https://script.google.com/macros/s/AKfycbyzr-3B2r3OGOM6LN6JiRBL9igI8g6Jod6PLTfOfuSVt8EIfUQaO4P8I6rlt3RCURboaQ/exec'
 
 const API_KEY = 'CONTACT_FORM_2025';
 
